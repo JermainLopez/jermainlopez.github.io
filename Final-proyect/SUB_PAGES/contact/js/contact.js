aeda458c677fd1068742efd7a86b705da74d88c5
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-04-06 13:48:38 -0600

    fixed

---
+++ Final-proyect/SUB_PAGES/contact/js/contact.js
@@ -0,0 +1,22 @@
+const myJSonCuzmelFile = '../../DATA/events.json';
+fetch(myJSonCuzmelFile)
+    .then(function (reponseFile) {
+        return reponseFile.json();
+    })
+    .then(function (myJSonlocalObject) {
+        console.table(myJSonlocalObject); 
+        const towns = myJSonlocalObject['cuzmel'].filter((towns) => {
+            if(towns.name==="Honda PCX150 Scooter") {
+                return towns;
+            }
+        });
+        console.log(towns);
+        for(let y=0; y<towns[0].events.length; y++) {
+            let card = document.createElement('section');
+            card.classList.add('ur');
+            let upevent = document.createElement('p');
+            upevent.textContent = towns[0].events[y];
+            card.appendChild(upevent);
+            document.querySelector('div.upcoming1').appendChild(card);
+        }
+    })
--- Final-proyect/SUB_PAGES/rentals/js/rental.js
+++ Final-proyect/SUB_PAGES/rentals/js/rental.js
@@ -46,10 +46,10 @@ function main(){
     var idx = rentalList.indexOf(rental);
    //document.querySelector('.flex1').innerText = idx;
    if (rental == undefined){
-    document.querySelector('.fiveday').innerText = "Please complete the information above"; 
+    document.querySelector('.fivedy').innerText = "Please complete the information above"; 
 
    }else{
-    document.querySelector('.fiveday').innerText = rental; 
+    document.querySelector('.fivedy').innerText = rental; 
     if (typeOfReservation == "reservation"){
         document.querySelector('.flex2').innerText = "Reservation"; 
         if (selectTime == "halfDay"){
@@ -93,3 +93,27 @@ function main(){
     
 
 }
+
+
+const myJSonCuzmelFile = '../../DATA/events.json';
+fetch(myJSonCuzmelFile)
+    .then(function (reponseFile) {
+        return reponseFile.json();
+    })
+    .then(function (myJSonlocalObject) {
+        console.table(myJSonlocalObject); 
+        const towns = myJSonlocalObject['cuzmel'].filter((towns) => {
+            if(towns.name==="Honda PCX150 Scooter") {
+                return towns;
+            }
+        });
+        console.log(towns);
+        for(let y=0; y<towns[0].events.length; y++) {
+            let card = document.createElement('section');
+            card.classList.add('ur');
+            let upevent = document.createElement('p');
+            upevent.textContent = towns[0].events[y];
+            card.appendChild(upevent);
+            document.querySelector('div.upcoming').appendChild(card);
+        }
+    })
